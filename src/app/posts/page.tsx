7c984f0e228a6cffd505a30beb934c5dcfed05f4
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-16 01:58:41 -0600

    fetch and display published posts on the Posts page with count

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,8 +1,33 @@
+import prisma from "@/lib/db";
+import Link from "next/link";
+
 export default async function PostsPage() {
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+    },
+  });
+
+  const postsCount = await prisma.post.count();
+
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-3xl font-semibold">All Posts (0) </h1>
-      <ul className="border-t border-b border-black/10 py-5 leading-8"></ul>
+      <h1 className="text-3xl font-semibold">All Posts ({postsCount}) </h1>
+      <ul className="border-t border-b border-black/10 py-5 leading-8">
+        {posts.map((post) => (
+          <li key={post.id} className="flex items-center justify-between">
+            <Link href={`/posts/${post?.slug}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
