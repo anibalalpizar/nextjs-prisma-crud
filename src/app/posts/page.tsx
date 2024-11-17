78019021c3691d20b033c886b0ce9519f2de9881
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-16 23:29:36 -0600

    update PostsPage to display post count dynamically and add form for creating new posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,26 +1,13 @@
+import { createPost } from "@/actions/actions";
 import prisma from "@/lib/db";
 import Link from "next/link";
 
 export default async function PostsPage() {
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-    select: {
-      id: true,
-      title: true,
-      slug: true,
-    },
-  });
-
-  const postsCount = await prisma.post.count();
+  const posts = await prisma.post.findMany();
 
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-3xl font-semibold">All Posts ({postsCount}) </h1>
+      <h1 className="text-3xl font-semibold">All Posts ({posts.length}) </h1>
       <ul className="border-t border-b border-black/10 py-5 leading-8">
         {posts.map((post) => (
           <li key={post.id} className="flex items-center justify-between">
@@ -28,6 +15,23 @@ export default async function PostsPage() {
           </li>
         ))}
       </ul>
+
+      <form action={createPost} className="flex flex-col gap-y-2 w-[300px]">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="px-2 py-1 rounded-sm"
+        />
+        <textarea
+          name="content"
+          placeholder="Content"
+          className="px-2 py-1 rounded-sm"
+        />
+        <button type="submit" className="bg-black text-white rounded-sm py-2">
+          Create Post
+        </button>
+      </form>
     </main>
   );
 }
